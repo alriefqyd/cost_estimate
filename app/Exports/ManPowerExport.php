2bea95b16465f0ec1fb10e02a77182205a5c6bd7
Al Riefqy <ekydesmito@gmail.com>
2024-10-29 16:11:20 +0800

    update currency usd to idr

--- app/Exports/ManPowerExport.php
+++ app/Exports/ManPowerExport.php
@@ -39,7 +39,7 @@ class ManPowerExport implements FromView, WithColumnFormatting,
         $columns = range('E', 'R');
 
         foreach ($columns as $column) {
-            $columnFormats[$column] = NumberFormat::FORMAT_CURRENCY_USD;
+            $columnFormats[$column] = NumberFormat::FORMAT_CURRENCY_IDR;
         }
 
         return $columnFormats;
